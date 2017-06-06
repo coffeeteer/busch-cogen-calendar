e79b7cbfe0945c5560a677ae05def2aa2f40f803
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-05 21:57:22 -0400

    created calendar element using AUI Scheduler

---
+++ public/javascript/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	YUI().use(
+	  'aui-scheduler',
+	  function(Y) {
+	    var events = [
+	      {
+	        content: 'Travis G',
+	        startDate: new Date(2017, 5, 5, 8 ),
+	        endDate: new Date(2017, 5, 5, 4 ),
+	      },
+	      {
+	      	content: 'Lenny S',
+	      	startDate: new Date(2017, 5, 5, 8),
+	      	endDate: new Date(2017, 5, 5, 4)
+	      }
+	    ];
+
+	    var agendaView = new Y.SchedulerAgendaView();
+	    var dayView = new Y.SchedulerDayView();
+	    var weekView = new Y.SchedulerWeekView();
+	    var monthView = new Y.SchedulerMonthView();
+
+	    new Y.Scheduler(
+	      {
+	        activeView: monthView,
+	        boundingBox: '#myScheduler',
+	        date: Date.now(),
+	        items: events,
+	        render: true,
+	        views: [dayView, weekView, monthView, agendaView]
+	      }
+	    );
+	  }
+	);
+}); 
