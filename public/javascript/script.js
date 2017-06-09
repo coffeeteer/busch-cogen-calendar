d86868c837631e723dc199676e087e31ba569aea
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-09 18:20:31 -0400

    still struggling

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,41 +1,7 @@
-$(document).ready(function(){
-	// YUI().use(
-	//   'aui-scheduler',
-	//   function(Y) {
-	//     var events = [
-	//       {
-	//         content: 'Travis G',
-	//         startDate: new Date(2017, 5, 5, 8 ),
-	//         endDate: new Date(2017, 5, 5, 4 )
-	//       },
-	//       {
-	//       	content: 'Lenny S',
-	//       	startDate: new Date(2017, 5, 5, 8),
-	//       	endDate: new Date(2017, 5, 5, 4)
-	//       }
-	//     ];
-
-	//     var agendaView = new Y.SchedulerAgendaView();
-	//     var dayView = new Y.SchedulerDayView();
-	//     var weekView = new Y.SchedulerWeekView();
-	//     var monthView = new Y.SchedulerMonthView();
-
-	//     new Y.Scheduler(
-	//       {
-	//         activeView: monthView,
-	//         boundingBox: '#myScheduler',
-	//         date: Date.now(),
-	//         items: events,
-	//         render: true,
-	//         views: [dayView, weekView, monthView, agendaView]
-	//       }
-	//     );
-	//   }
-	// );
-
-var calendar = $("#calendar").calendar(
-	{
-		tmpl_path: "/tmpls/",
-		events_source: function () { return []; }
-	});	
-}); 
+// $(document).ready(function(){
+	// var calendar = $("#calendar").calendar(
+	// 	{
+	// 		tmpl_path: "/tmpls/",
+	// 		events_source: function () { return []; }
+	// 	});	
+// }); 
