073e147828e9971490baa290f1b665d54b64a3fb
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-16 23:13:37 -0400

    working on configuring the database

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 
 	var events = [
-		{id: 1, text: 'Travis G', start_date: '06/16/2017 16:00', end_date: '06/17/2017 00:00'},
-		{id: 2, text: 'Glenn H', start_date: '06/16/2017 16:15', end_date: '06/17/2017 08:00'}
+		{id: 1, text: 'Travis G', start_date: '2017/06/16 16:00', end_date: '2017/06/17 00:00'},
+		{id: 2, text: 'Glenn H', start_date: '2017/06/16 16:15', end_date: '2017/06/17 08:00'}
 	];
 
+	// CREATE TABLE `events` (
+	//   `id` int(11) NOT NULL AUTO_INCREMENT,
+	//   `start_date` datetime NOT NULL,
+	//   `end_date` datetime NOT NULL,
+	//   `text` varchar(255) NOT NULL,
+	//   PRIMARY KEY (`id`)
+	// )
 
 	(function init() {
-     scheduler.init('scheduler_here', new Date(), 'month'); // Starts with today's date
+		scheduler.config.xml_date='%Y-%m-%d %H:%i';
+
+    	scheduler.init('scheduler_here', new Date(), 'month'); // Starts with today's date
 
 	    scheduler.parse(events, 'json');
     })();
